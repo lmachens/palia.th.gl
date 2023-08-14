515f83cb4fcb6e7a53408a453af05cb4a069651f
lmachens <leon@machens.koeln>
2023-08-14 15:07:40 +0200

    Optimize twitch embed

--- app/components/twitch-embed.tsx
+++ app/components/twitch-embed.tsx
@@ -3,11 +3,6 @@ import { trackEvent } from "./plausible-tracker";
 
 declare global {
   interface Window {
-    onYouTubeIframeAPIReady: () => void;
-    YT: {
-      Player: any;
-      PlayerState: any;
-    };
     Twitch: any;
   }
 }
@@ -23,13 +18,50 @@ export default function TwitchEmbed({ onClose }: { onClose: () => void }) {
       autoplay: true,
       muted: true,
       parent: ["diablo4.th.gl"],
-      quality: "mobile",
+      quality: "160p30",
     });
 
     twitchEmbed.addEventListener(window.Twitch.Player.ONLINE, () => {
       trackEvent("Ad Fallback: Twitch", {
         props: { url: `https://www.twitch.tv/${CHANNEL}` },
       });
+    });
+
+    const REFRESH_INTERVAL = 1000 * 60;
+    let lastUpdate = Date.now();
+    let timeout = setTimeout(refreshTwitchEmbed, REFRESH_INTERVAL);
+    function refreshTwitchEmbed() {
+      lastUpdate = Date.now();
+      if (
+        !(
+          twitchEmbed.getPlayerState()?.playback === "Playing" &&
+          twitchEmbed.getDuration() === 0
+        )
+      ) {
+        twitchEmbed.setChannel(
+          [...twitchEmbed.getChannel()]
+            ?.map((char) =>
+              char === char.toUpperCase()
+                ? char.toLowerCase()
+                : char.toUpperCase()
+            )
+            .join("")
+        );
+      }
+      timeout = setTimeout(refreshTwitchEmbed, REFRESH_INTERVAL);
+    }
+
+    window.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "visible") {
+        const timeLeft = REFRESH_INTERVAL - (Date.now() - lastUpdate);
+        if (timeLeft < 0) {
+          refreshTwitchEmbed();
+        } else {
+          timeout = setTimeout(refreshTwitchEmbed, timeLeft);
+        }
+      } else {
+        clearTimeout(timeout);
+      }
     });
   };
 
