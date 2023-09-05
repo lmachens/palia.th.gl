ae652cc31104afe921269a678c2d35955e411906
lmachens <leon@machens.koeln>
2023-09-05 10:07:45 +0200

    Fix static rendering

--- app/(web)/[lang]/[map]/nodes/[name]/[coordinates]/page.tsx
+++ app/(web)/[lang]/[map]/nodes/[name]/[coordinates]/page.tsx
@@ -1,14 +1,5 @@
-import { LOCALES } from "@/app/lib/i18n";
-import { CONFIGS } from "@/app/lib/maps";
-
 export { generateMetadata } from "@/app/lib/meta";
 
 export default function Empty() {
   return <></>;
-}
-
-export async function generateStaticParams() {
-  return LOCALES.flatMap((locale) =>
-    Object.keys(CONFIGS).map((map) => ({ locale, map }))
-  );
 }
--- app/(web)/[lang]/[map]/page.tsx
+++ app/(web)/[lang]/[map]/page.tsx
@@ -1,3 +1,12 @@
+import { LOCALES } from "@/app/lib/i18n";
+import { CONFIGS } from "@/app/lib/maps";
+
 export default function Empty() {
   return <></>;
+}
+
+export async function generateStaticParams() {
+  return LOCALES.flatMap((locale) =>
+    Object.keys(CONFIGS).map((map) => ({ locale, map }))
+  );
 }
