0f98cd7b822ef3edfa5ab9c5302a803a42decf0d
lmachens <leon@machens.koeln>
2023-09-05 10:10:47 +0200

    Remove static params

--- app/(web)/[lang]/[map]/page.tsx
+++ app/(web)/[lang]/[map]/page.tsx
@@ -1,12 +1,3 @@
-import { LOCALES } from "@/app/lib/i18n";
-import { CONFIGS } from "@/app/lib/maps";
-
 export default function Empty() {
   return <></>;
-}
-
-export async function generateStaticParams() {
-  return LOCALES.flatMap((locale) =>
-    Object.keys(CONFIGS).map((map) => ({ locale, map }))
-  );
 }
