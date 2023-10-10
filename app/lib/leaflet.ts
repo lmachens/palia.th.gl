9f1eefa46079151c59a711072be807b801825eaf
lmachens <leon@machens.koeln>
2023-10-10 16:16:31 +0200

    Fix type of import

--- app/lib/leaflet.ts
+++ app/lib/leaflet.ts
@@ -1,5 +1,5 @@
 export let leaflet: // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-| typeof import("/home/lmachens/palia.th.gl/node_modules/.pnpm/@types+leaflet@1.9.6/node_modules/@types/leaflet/index");
+typeof import("leaflet");
 export type * from "leaflet";
 async function importLeaflet() {
   const Leaflet = (await import("leaflet")).default;
