7113527d13b98845e277f16a2e4c2bc300f7a790
lmachens <leon@machens.koeln>
2023-10-11 16:23:54 +0200

    Fix overwolf check

--- app/api/weekly-wants/route.ts
+++ app/api/weekly-wants/route.ts
@@ -5,17 +5,14 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 async function _GET(request: NextRequest) {
-  const locale = request.nextUrl.searchParams.get("locale") ?? DEFAULT_LOCALE;
-  if (
-    !request.headers.get("User-Agent")?.includes("OverwolfClient") ||
-    !request.headers.get("Origin")?.includes("overwolf-extension")
-  ) {
+  const locale = request.nextUrl.searchParams.get("locale") || DEFAULT_LOCALE;
+  if (!request.headers.get("User-Agent")?.includes("OverwolfClient")) {
     return NextResponse.json({ error: "Not Overwolf" }, { status: 400 });
   }
   const result = await fetchWeeklyWants(locale);
   return NextResponse.json(result, {
     headers: {
-      "Access-Control-Allow-Origin": request.headers.get("Origin")!,
+      "Access-Control-Allow-Origin": "*",
     },
   });
 }
