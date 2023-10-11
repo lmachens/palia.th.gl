9cfcb185adead4c3927c94e497c771d5b126c1b6
lmachens <leon@machens.koeln>
2023-10-11 16:09:30 +0200

    Bump version

--- app/api/weekly-wants/route.ts
+++ app/api/weekly-wants/route.ts
@@ -6,11 +6,18 @@ import { NextResponse } from "next/server";
 
 async function _GET(request: NextRequest) {
   const locale = request.nextUrl.searchParams.get("locale") ?? DEFAULT_LOCALE;
-  if (!request.headers.get("User-Agent")?.includes("OverwolfClient")) {
+  if (
+    !request.headers.get("User-Agent")?.includes("OverwolfClient") ||
+    !request.headers.get("Origin")?.includes("overwolf-extension")
+  ) {
     return NextResponse.json({ error: "Not Overwolf" }, { status: 400 });
   }
   const result = await fetchWeeklyWants(locale);
-  return NextResponse.json(result);
+  return NextResponse.json(result, {
+    headers: {
+      "Access-Control-Allow-Origin": request.headers.get("Origin")!,
+    },
+  });
 }
 
 export const GET = isOverwolf ? undefined : _GET;
