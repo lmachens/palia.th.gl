463498514a7f51522cf29a90d1da95ebba4d2115
lmachens <leon@machens.koeln>
2023-12-11 14:46:34 +0100

    Fix SVG props

--- components/web-header.tsx
+++ components/web-header.tsx
@@ -131,11 +131,11 @@ export default function WebHeader({
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M10 4l2 1l2 -1" />
